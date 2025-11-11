fc707b8d8f306781e02aad56555841b0677a5786
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-11 08:32:10 +0200

    Create Counter.jsx
    declare state
    create click handler callback

---
+++ component-optimization/src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Counter(){
+
+    const [count, setCount] = useState(0)
+
+    const clickCounterHandler = () => {
+        setCount(count => count + 1)
+    }
+
+    return (
+        <>
+        <h2>Counter - {count}</h2>
+
+        <button onClick={() => clickCounterHandler()}>+</button>
+        </>
+    );
+}
