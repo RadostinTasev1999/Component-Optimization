1deff5d7b60c9e92aa9bdd0f77949d714c40c101
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-12 08:21:21 +0200

    Update Counter.jsx
    Bind click handler to button
    manage state in clickCounterHandler

--- component-optimization/src/components/Counter.jsx
+++ component-optimization/src/components/Counter.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react"
 
-export default function Counter(){
+export default function Counter({ count, setCount}){
 
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0);
 
     const clickCounterHandler = () => {
         setCount(count => count + 1)
@@ -12,7 +12,7 @@ export default function Counter(){
         <>
         <h2>Counter - {count}</h2>
 
-        <button onClick={() => clickCounterHandler()}>+</button>
-        </>
-    );
+        <button onClick={clickCounterHandler}>+</button>
+        </>      
+    )
 }
