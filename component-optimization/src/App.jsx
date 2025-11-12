b7eb658c53c7fa1b9a40510b73f7f736b9a885ea
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-12 08:19:30 +0200

    Update App.jsx
    declare state
    pass state and setState to Counter component via props

--- component-optimization/src/App.jsx
+++ component-optimization/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import TodoList from './components/TodoList'
+import Counter from './components/Counter'
+import { useState } from 'react'
 
 function App() {
+
+  const [count, setCount] = useState(0)
   
   return (
     <>
       <h1>ToDo List</h1>
+
       <TodoList />
+
+      <Counter count={count} setCount={setCount} />
     </>
   )
 }
