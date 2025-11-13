48c5a71caac46ac8d4a0da1e0e41565ae44dd0c4
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-13 08:34:11 +0200

    Update Counter.jsx
    Use React.memo() in order to memoize Counter component

--- component-optimization/src/components/Counter.jsx
+++ component-optimization/src/components/Counter.jsx
@@ -1,18 +1,28 @@
 // import { useState } from "react"
+import React from 'react'
 
-export default function Counter({ count, setCount}){
+function Counter({ count, increment}){
 
     // const [count, setCount] = useState(0);
 
-    const clickCounterHandler = () => {
-        setCount(count => count + 1)
-    }
+  
 
     return (
         <>
         <h2>Counter - {count}</h2>
 
-        <button onClick={clickCounterHandler}>+</button>
+        <button onClick={() => increment()}>+</button>
         </>      
     )
 }
+
+
+export default React.memo(Counter);
+/*
+We wrap the component in memo to get a memoized version of that component
+This returns a new React component
+It behaves the same as the component provided to memo except that React will not 
+always re-render it when its parent is being re-rendered unless its props have changes
+Memoized components skip re-renders during parent updates if their new
+props are the same as the old props
+*/
