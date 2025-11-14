2b3cdb8893b6089bbd21ef5cf5a0604d6b9234e9
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-14 08:35:14 +0200

    Update TodoList
    use UseCallback in order to cache function definition between re-renders

--- component-optimization/src/components/TodoList.jsx
+++ component-optimization/src/components/TodoList.jsx
@@ -1,6 +1,7 @@
 import TodoItem from "./TodoItem"
-import { useState } from "react"
+import { useState, useCallback } from "react"
 import React from 'react'
+
 
 const initialTodos = [
     { id: 1,text: 'Do homework', isCompleted: false},
@@ -12,11 +13,11 @@ function TodoList(){
 
     const [ todos, setTodos ] = useState(initialTodos)
 
-    const toggleTodoHandler = (todoId) => {
+    const toggleTodoHandler = useCallback((todoId) => {
         console.log('TodoId is:', todoId )
 
         setTodos(state => state.map(todo => todoId === todo.id ? {...todo, isCompleted: !todo.isCompleted} : todo))
-    }
+    },[])
 
     return (
         <>
@@ -24,6 +25,17 @@ function TodoList(){
             <ul>
                 {
                   todos.map(todo => <TodoItem {...todo} onToggle={toggleTodoHandler} />)
+                  /*
+                  memo(<calculated value>) 
+                  calculated value - the function calculating the value that you want to cache.
+                                   - React will call your function during the initial render
+                                   - On next renders, React will return the same value again, if the
+                                     dependencies have not changed since the last render.
+                                   - Otherwise, it will call calculateValue, return its result and store it
+                                     so it can be re-used later.
+
+
+                  */
                 }
             </ul>
         </>
