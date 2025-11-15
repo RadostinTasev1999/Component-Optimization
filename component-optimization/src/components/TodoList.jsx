3e22294c19780cf089484b6d1f069b677cfd1229
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-15 08:47:24 +0200

    Update TodoList.jsx
    use useMemo to cache the result of calculation of heavyCalculation function
    render the returned value of heavyCalculation

--- component-optimization/src/components/TodoList.jsx
+++ component-optimization/src/components/TodoList.jsx
@@ -1,6 +1,7 @@
 import TodoItem from "./TodoItem"
 import { useState, useCallback } from "react"
 import React from 'react'
+import { useMemo } from "react"
 
 
 const initialTodos = [
@@ -19,25 +20,45 @@ function TodoList(){
         setTodos(state => state.map(todo => todoId === todo.id ? {...todo, isCompleted: !todo.isCompleted} : todo))
     },[])
 
+/*
+    useMemo is a React Hook that lets you cache result of a calculation between re-renders.
+*/
+    const heavyCalculation = useMemo(() => {
+      //Heavy calculation
+
+      function pauseComp(ms) {
+        var curr = new Date().getTime(); //It gets the current time in milliseconds since the Unix epoch and stores it in the curr variable.
+        ms += curr; // It calculates the target end time by adding the desired pause duration (ms) to the current time (curr).
+        while(curr < ms){
+          curr = new Date().getTime()
+        }
+        /*
+        This is the core of the pause mechanism. The program enters a while loop that continuously checks the current time. 
+        It stays in this loop, doing nothing but repeatedly fetching the current time, until the current time (curr) is equal to or greater than the calculated end time (ms).
+        */
+      }
+
+      pauseComp(1000)
+      /*
+      The line pauseComp(1000) calls the function to pause the program's execution for 1000 milliseconds (1 second).
+      */
+
+      return 42;
+    },[])
+
+    
+
+
     return (
         <>
         <h2>Todos</h2>
             <ul>
                 {
                   todos.map(todo => <TodoItem {...todo} onToggle={toggleTodoHandler} />)
-                  /*
-                  memo(<calculated value>) 
-                  calculated value - the function calculating the value that you want to cache.
-                                   - React will call your function during the initial render
-                                   - On next renders, React will return the same value again, if the
-                                     dependencies have not changed since the last render.
-                                   - Otherwise, it will call calculateValue, return its result and store it
-                                     so it can be re-used later.
-
-
-                  */
                 }
             </ul>
+
+            <p>Meaning of life: { heavyCalculation }</p>
         </>
    )
 }
